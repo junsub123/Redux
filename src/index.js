bf2eecc219d1a3f2aa6a796157fff87eb2b852c3
subsub <89231026+junsub123@users.noreply.github.com>
2023-05-26 10:53:38 +0900

    commit
    commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import React_redux from './React_redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <React_redux />
+    <App />
   </React.StrictMode>
 );
 
